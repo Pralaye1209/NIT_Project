c13c1e86c9ecb98c9364955ff98d06a799f294e7
Praful_Yerojwar Pralaye <99270041+Pralaye1209@users.noreply.github.com>
2022-05-04 16:53:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
 
 
-
     $('.input-group input[required], .input-group textarea[required], .input-group select[required]').on('keyup change', function () {
         var $form = $(this).closest('form'),
                 $group = $(this).closest('.input-group'),
                 $addon = $group.find('.input-group-addon'),
                 $icon = $addon.find('span'),
                 status = false;
+
         if (!$group.data('validate')) {
             status = $(this).val() ? true : false;
         } else if ($group.data('validate') == "email") {
@@ -43,89 +43,5 @@ $(document).ready(function () {
     });
 
     $('.input-group input[required], .input-group textarea[required], .input-group select[required]').trigger('change');
-
-    var $panel = $(this).parents('.filterable'),
-            $filters = $panel.find('.filters input'),
-            $tbody = $panel.find('.table tbody');
-
-
-    if ($filters.prop('disabled') == true) {
-        $filters.prop('disbaled', false);
-        $filters.first().focus();
-    } else {
-        $filters.val('').prop('disabled', true);
-        $tbody.find('no-result').remove();
-        $tbody.find('tr').show();
-    }
-
-    $('.filterable .filters input').keyup(function (e) {
-
-        var code = e.keyCode || e.which;
-        if (code == 9)
-            return;
-        var $input = $(this),
-                inputContent = $input.val().toLowerCase(),
-                $panel = $input.parents('.filterable'),
-                column = $panel.find('.filters th').index($input.parents('th')),
-                $table = $panel.find('.table'),
-                $rows = $table.find('tbody tr');
-
-        var $filteredRows = $rows.filter(function () {
-            var value = $(this).find('td').eq(column).text().toLowerCase();
-
-            return value.indexOf(inputContent) === -1;
-        });
-
-        $table.find('tbody .no-result').remove();
-        $rows.show();
-        $filteredRows.hide();
-
-        if ($filteredRows.length === $rows.length) {
-            $table
-                    .find('tbody')
-                    .prepend(
-                            $('<tr class="no-result text-center text-warning"><td colspan="'
-                                    + $table
-                                    .find('.filters th').length
-                                    + '"><strong>!! No Records Found !!</strong></td></tr>'));
-
-
-        }
-    });
-
-    $('.filterable .btn-filter').click(function () {
-        var $panel = $(this).parents('.filterable'),
-                $filters = $panel.find('.filters input'),
-                $tbody = $panel.find('.table tbody');
-        if ($filters.prop('disabled') == true) {
-            $filters.prop('disabled', false);
-            $filters.first().focus();
-        } else {
-            $filters.val('').prop('disabled', true);
-            $tbody.find('.no-result').remove();
-            $tbody.find('tr').show();
-        }
-        return false;
-    });
-
-
-
-
-  $("#confirmpassword").change(function(){
-  
-    if($("#newpassword").val()==$("#confirmpassword").val()){
-      $("#error").hide();
-      
-      
-    }
-    else{
-      $("#error").html("<strong>* New and Confirm Password should be same.</strong>");
-      $("#error").show();
-    }
-    
-  
-});
-
-                                                        
 
 });
---
+++ model.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $("#login").click(function(){
+            $("#myrequesttable").show();
+            $("#myviewdetails").hide();
+            $("#myupdatestatus").hide();
+            $("#myadd").hide();
+        });
+        
+    
+    
+    
+    });
